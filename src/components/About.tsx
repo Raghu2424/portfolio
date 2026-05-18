c3e58715b1868c4ddcb4ce77d26285e347583ed5
Raghavendra <raghavendrakshetri@gmail.com>
2026-05-18 23:14:48 +0530

    changes career

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,8 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          I am currently founder of FuturVia, India's First Mobile-First, Zero-Fee Tech Platform. I am a technologist at heart with strong business judgment—
-          FuturVia removes every barrier between you and a tech career.Dedicated placement support with partner companies. Mock interviews, resume reviews, and job referrals included.
-        </p>
+          I am a 2nd-year Computer Science Engineering student passionate about Artificial Intelligence, Web Development, Open Source, and UI/UX Design.
+          I enjoy building projects, participating in hackathons, and exploring modern technologies through practical learning and real-world experiences. </p>
       </div>
     </div>
   );
